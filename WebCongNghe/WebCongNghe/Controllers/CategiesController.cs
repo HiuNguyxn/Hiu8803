b1e7432c774a20a3df942de6f5ec87739d0946f6
HiuNguyxn <hieu882003@gmail.com>
2022-10-21 21:03:52 +0700

    first commit

---
+++ WebCongNghe/WebCongNghe/Controllers/CategiesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebCongNghe.Models;
+
+namespace WebCongNghe.Controllers
+{
+    public class CategiesController : Controller
+    {
+        DBWebEntities db = new DBWebEntities();
+        // GET: Categies
+        public ActionResult Index()
+        {
+            return View(db.Categories.ToList());
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Category cate)
+        {
+            db.Categories.Add(cate);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            return View(db.Categories.Where(m => m.Id == id).FirstOrDefault());
+        }
+        [HttpPost]
+        public ActionResult Delete(int id, Category cate)
+        {
+            cate = db.Categories.Where(m => m.Id == id).FirstOrDefault();
+            db.Categories.Remove(cate);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            return View(db.Categories.Where(m => m.Id == id).FirstOrDefault());
+        }
+        [HttpPost]
+        public ActionResult Edit(int id, Category cate)
+        {
+            db.Entry(cate).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebCongNghe/WebCongNghe/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebCongNghe.MvcApplication" Language="C#" %>
